4612ad3897c50b4f714f1b25f4064f4be31feabd
hashimotz07 <lucasissamuhashimoto@gmail.com>
2025-05-12 16:54:14 -0300

    adicao da funcao de permutacao

--- sdes.cpp
+++ sdes.cpp
@@ -1,14 +1,25 @@
 #include <bits/stdc++.h>
-#define int long long
 #define inacio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 
-int32_t main(){
+/*permuta*/
+int permuta(int n, vector<int> v, int total_bits){
+    int resultado = 0;
+    for(int i = 0; i < v.size(); i++){
+        resultado = resultado << 1;
+        resultado |= (n >> (total_bits - v[i])) & 1;
+    }
+    return resultado;
+}
+
+int main(){
     inacio;
     int chave = 1010000010; //chave de 10 bits
     int dado = 11010111; //bloco de dados de 8 bits
 
-    
+    /*geração de chaves subjacentes*/
+
+
 
     return 0;
 }
