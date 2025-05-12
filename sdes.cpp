bca1a4537c2f89c29543f8595821ded2a85080e0
hashimotz07 <lucasissamuhashimoto@gmail.com>
2025-05-12 16:40:05 -0300

    primeiro commit

---
+++ sdes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+#define inacio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int32_t main(){
+    inacio;
+    int chave = 1010000010; //chave de 10 bits
+    int dado = 11010111; //bloco de dados de 8 bits
+
+    
+
+    return 0;
+}
